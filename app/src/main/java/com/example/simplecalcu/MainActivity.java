02fbf7fdcdbd2d2d9800822d1534e05e59b3eda5
ShinAquila <augusto08rivera12@gmail.com>
2024-04-01 14:27:34 +0800

    added temp txt

--- app/src/main/java/com/example/simplecalcu/MainActivity.java
+++ app/src/main/java/com/example/simplecalcu/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.simplecalcu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void num_input(View v){
+        Button num1 = (Button) findViewById(R.id.num1BTN);
+
+        EditText input_output = (EditText)findViewById(R.id.input_output);
+        TextView tempNums = (TextView) findViewById(R.id.tempNums);
+
+        int num = Integer.parseInt(num1.getText().toString());
+
+        input_output.setText(""+num);
     }
 }
